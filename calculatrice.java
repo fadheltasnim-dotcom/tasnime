ff1a3e782f009f2acc15cc97e9345e9aa19051a0
tasnim <fadheltasnim@gmail.com>
2025-10-20 10:29:47 +0200

    Add files via upload

---
+++ calculatrice.java
@@ -0,0 +1,10 @@
+package tp2;
+
+public class calculatrice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ paire.java
@@ -0,0 +1,10 @@
+package tp2;
+
+public class paire {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
