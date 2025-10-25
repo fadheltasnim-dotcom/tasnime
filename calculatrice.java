79ebb2e0f4b0fe4fa714d525d79ba3e7415f0ab9
tasnim <fadheltasnim@gmail.com>
2025-10-25 22:40:53 +0200

    Implement basic calculator functionality
    calculatrice

--- calculatrice.java
+++ calculatrice.java
@@ -1,10 +1,48 @@
 package tp2;
-
+import java.util.Scanner;
 public class calculatrice {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	  public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
 
-	}
+	        double a, b;
+	        char operateur;
+
+	        System.out.print("Entrez le premier nombre : ");
+	        a = sc.nextDouble();
+
+	        System.out.print("Entrez l'opérateur (+, -, *, /) : ");
+	        operateur = sc.next().charAt(0);
+
+	        System.out.print("Entrez le deuxième nombre : ");
+	        b = sc.nextDouble();
+
+	        double resultat;
+
+	        // Vérification de l’opérateur
+	        if (operateur == '+') {
+	            resultat = a + b;
+	            System.out.println("Résultat : " + resultat);
+	        } else if (operateur == '-') {
+	            resultat = a - b;
+	            System.out.println("Résultat : " + resultat);
+	        } else if (operateur == '*') {
+	            resultat = a * b;
+	            System.out.println("Résultat : " + resultat);
+	        } else if (operateur == '/') {
+	            if (b != 0) {
+	                resultat = a / b;
+	                System.out.println("Résultat : " + resultat);
+	            } else {
+	                System.out.println("Erreur : division par zéro !");
+	            }
+	        } else {
+	            System.out.println("Erreur : opérateur invalide !");
+	        }
+
+	        
+	    }
+
+
 
 }
