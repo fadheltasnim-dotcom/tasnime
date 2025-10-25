591a3281c8c54485f26763913f75203ae060cc22
tasnim <fadheltasnim@gmail.com>
2025-10-25 23:01:20 +0200

    Update test.java
    test de la validité de la cercle et de la rectangle

--- test.java
+++ test.java
@@ -1,10 +1,21 @@
 package tp2;
 
+import java.util.Scanner;
+
 public class test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        cercle c = new cercle();
+        System.out.print("Entrez le rayon du cercle : ");
+        c.rayon = sc.nextDouble();
+        System.out.println("La surface du cercle est : " + c.surface());
 
-	}
-
+        rectangle r = new rectangle();
+        System.out.print("Entrez la longueur du rectangle : ");
+        r.longueur = sc.nextDouble();
+        System.out.print("Entrez la largeur du rectangle : ");
+        r.largeur = sc.nextDouble();
+        System.out.println("La surface du rectangle est : " + r.surface());
+    }
 }
