7e9fe3d32bb0cb801a7f1399f1e2a11ee28aeae7
tasnim <fadheltasnim@gmail.com>
2025-10-25 22:56:51 +0200

    Update rectangle.java
    rectangle

--- rectangle.java
+++ rectangle.java
@@ -1,5 +1,19 @@
 package tp2;
 
 public class rectangle {
+    double longueur;
+    double largeur;
 
+    public rectangle() { }
+
+  
+    public rectangle(double longueur, double largeur) {
+        this.longueur = longueur;
+        this.largeur = largeur;
+    }
+
+ 
+    double surface() {
+        return longueur * largeur;
+    }
 }
