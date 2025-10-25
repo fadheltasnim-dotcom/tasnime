cc14cbc17856dc30e22e87f6f52aa00f22e49e47
tasnim <fadheltasnim@gmail.com>
2025-10-25 22:31:00 +0200

    Implement leap year check in bissectrice.java
    Add functionality to check if a given year is a leap year.

--- bissectrice.java
+++ bissectrice.java
@@ -1,5 +1,21 @@
 package tp2;
-
+import java.util.Scanner;
 public class bissectrice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Entrez une année : ");
+        int annee = sc.nextInt();
+
+       
+        if ((annee % 400 == 0) || (annee % 4 == 0 && annee % 100 != 0)) {
+            System.out.println(annee + " est une année bissextile.");
+        } else {
+            System.out.println(annee + " n'est pas une année bissextile.");
+        }
+
+        sc.close();
+    }
 
 }
+
